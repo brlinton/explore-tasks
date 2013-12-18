59452aadca86d83c91bfbe53b0caafe6c02a23ab
Brandon Linton <linton.accounts@gmail.com>
2013-12-17 22:51:48 -0500

    First test: CurrentId

---
+++ ExploreTasks/ExploreTasks/TaskTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System.Linq;
+using Xunit;
+
+namespace ExploreTasks
+{
+    public class TaskTests
+    {
+        [Fact]
+        public void CurrentIdIsNullWhenNotExecutingATask()
+        {
+            var currentTaskId = Task.CurrentId;
+
+            // Since I'm not in a task
+            Assert.Equal(null, currentTaskId);
+        }
+    }
+}
